234846bfcda8424f84516d4fb6cc2790b1a27f1a
yumyv <yuriimyronets@gmail.com>
2019-12-03 06:45:04 +0200

    HW12-Functions: done

--- HW12-Functions/task1/main.js
+++ HW12-Functions/task1/main.js
@@ -1,3 +1,6 @@
+let countOf25 = 0;
+let countOf50 = 0;
+
 const isValidItems = (arr) => {
   const result = arr.map((e) => {
     return parseFloat(e);
@@ -7,36 +10,47 @@ const isValidItems = (arr) => {
   });
 };
 
-const isValidFor50 = (arr, prevItem, mainResult) => {
-  const isValidItem = arr.some((i) => {
-    return parseInt(i) === 25;
-  });
-  return mainResult ? !!((prevItem === 25) || (prevItem === 50 && isValidItem)) : false;
+const isValidFor50 = (mainResult) => {
+  if (mainResult) {
+    if (countOf25 > 0) {
+      countOf25--;
+      countOf50++;
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 };
 
-const isValidFor100 = (arr, mainResult) => {
-  let countOf25 = 0;
-  let countOf50 = 0;
-  arr.forEach((i) => {
-    if (parseInt(i) === 25) {
-      countOf25++;
+const isValidFor100 = (mainResult) => {
+  if (mainResult) {
+    if (countOf25 > 2) {
+      countOf25 -= countOf25 * 3;
+      return true;
+    } else if (countOf25 > 0 && countOf50 > 0) {
+      countOf25--;
+      countOf50--;
+      return true;
+    } else {
+      return false;
     }
-    if (parseInt(i) === 50) {
-      countOf50++;
-    }
-  });
-  return mainResult ? !!(countOf25 > 2 || (countOf25 > 0 && countOf50 > 0)) : false;
+  } else {
+    return false;
+  }
 };
 
 const isValidCalculate = (personAsArr) => {
   let result = true;
   for (let i = 0; i < personAsArr.length; i++) {
     if (parseInt(personAsArr[i]) === 25) {
+      countOf25++;
       result = result !== false;
     } else if (parseInt(personAsArr[i]) === 50) {
-      result = isValidFor50(personAsArr.slice(0, i + 1), parseInt(personAsArr[i - 1]), result);
+      result = isValidFor50(result);
     } else if (parseInt(personAsArr[i]) === 100) {
-      result = isValidFor100(personAsArr.slice(0, i + 1), result);
+      result = isValidFor100(result);
     }
   }
   return result;
@@ -52,4 +66,4 @@ const tickets = (person) => {
   return result;
 };
 
-document.write(tickets([25,'25',50]));
+document.write(tickets([25, '25', 50]));
