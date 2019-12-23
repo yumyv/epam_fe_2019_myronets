56115c91d71a67bf9e6dfe7c199e244a489f3452
yumyv <yuriimyronets@gmail.com>
2019-12-23 23:22:02 +0200

    HW18-Errors:done

--- HW18-Errors/task3/main.js
+++ HW18-Errors/task3/main.js
@@ -6,6 +6,8 @@ function parseJSON(json) {
   try {
     if (JSON.parse(json).name && JSON.parse(json).company) {
       console.log(JSON.parse(json));
+    } else {
+      console.log('error');
     }
   } catch {
     throw new Error();
